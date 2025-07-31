fe9a9d9361430fdbee0e04ef017bcb46709eab1d
Pankaj Mittal <433pankajmittal@gmail.com>
2025-07-31 23:30:07 +0530

    Update auth.controller.js

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -20,7 +20,7 @@ export const SignUp=async(req,res)=>{
        res.cookie("token", token, {
             httpOnly: true,
             secure: true,        // localhost के लिए false
-            sameSite: "lax",      // "none" की बजाय "lax" use करें
+            sameSite: "None",      // "none" की बजाय "lax" use करें
             path: "/",            // path add करें
             maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
         });
@@ -47,7 +47,7 @@ export const login=async(req,res)=>{
        res.cookie("token", token, {
             httpOnly: true,
             secure: true,        // localhost के लिए false
-            sameSite: "lax",      // "none" की बजाय "lax" use करें
+            sameSite: "None",      // "none" की बजाय "lax" use करें
             path: "/",            // path add करें
             maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
         });
