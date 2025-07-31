381f47c8a5c9ab7dc6aa957e012fb79f997ee2ad
Pankaj Mittal <433pankajmittal@gmail.com>
2025-07-31 21:59:16 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ let app=express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://a-cozy-comfort.onrender.com",
     credentials:true
 }))
 app.use('/api/auth',authrouter);
